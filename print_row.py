99507ae3325e4f1e03e7a4eb89f6ad058411eae7
Kyle Kato <kyle.kato@linxda.com>
2022-03-12 17:57:53 -0600

    Adding the election audit code.

---
+++ print_row.py
@@ -0,0 +1,4 @@
+# If the candidate does not match any existing candidate...
+if candidate_name not in candidate_options:
+    # Add it to the list of candidates.
+    candidate_options.append(candidate_name)
