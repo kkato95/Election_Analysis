c1e0c6097106a12d778de2aa72db45c74dc9832d
Kyle Kato <kyle.kato@linxda.com>
2022-03-13 22:03:16 -0500

    Removed 8 files

--- print_row.py
+++
@@ -1,4 +0,0 @@
-# If the candidate does not match any existing candidate...
-if candidate_name not in candidate_options:
-    # Add it to the list of candidates.
-    candidate_options.append(candidate_name)
